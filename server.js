0d05e7d5100c77a39f68be4d5ed8f2c2d045da88
unknown <Maksym Bliednov>
2018-07-23 15:27:12 -0400

    Testing

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ hbs.registerHelper('screamIt', (text) => {
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle: 'Mansur Idi Nahui',
+    pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to the page'
   });
 });
